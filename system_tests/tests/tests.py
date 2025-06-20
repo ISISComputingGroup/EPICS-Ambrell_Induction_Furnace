7e7a0d0189f249a2eeadb6f6257a38b4499fb20a
DavidKeymer <david.keymer@stfc.ac.uk>
2025-06-20 16:49:03 +0100

    Added commands and tests

--- system_tests/tests/tests.py
+++ system_tests/tests/tests.py
@@ -32,4 +32,12 @@ class Ambrell_Easy_HeatTests(unittest.TestCase):
 
     def test_WHEN_system_id_requested_THEN_value_returned(self):
         expected_system_id = "12345"
-        self.ca.assert_that_pv_is("ID", expected_system_id, timeout=10)
+        self.ca.assert_that_pv_is("ID", expected_system_id, timeout=10) # timeout to cover 10s SCAN of record.  Value returned in ~9s.
+
+    def test_WHEN_raw_data_requested_THEN_value_returned(self):
+        expected_raw_data = "1,1,280.0,280.0,1006,286,0,972"
+        self.ca.assert_that_pv_is("DATA:RAW", expected_raw_data, timeout=2) # timeout to cover 1s SCAN of record.  Value returned in <1s.
+
+    def test_WHEN_raw_status_requested_THEN_value_returned(self):
+        expected_raw_status = "1,2,3,4,5"
+        self.ca.assert_that_pv_is("STATUS:RAW", expected_raw_status, timeout=2) # timeout to cover 1s SCAN of record.  Value returned in <1s.
