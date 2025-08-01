fa3b01b16a4be64ea1a21cd4c18fae216b0bc8f2
DavidKeymer <david.keymer@stfc.ac.uk>
2025-08-01 17:42:49 +0100

    split values into fields, used backdoor, updated tests (failing)

--- system_tests/lewis_emulators/ambrell_easy_heat/device.py
+++ system_tests/lewis_emulators/ambrell_easy_heat/device.py
@@ -12,16 +12,26 @@ class SimulatedAmbrellEasyHeat(StateMachineDevice):
         """
         self.connected = True
 
+        self.id = 12345
+
+        # Values in data string:
+
         self.address = 3
 
         self.heat_status = 1
 
-        self.id = 12345
+        self.tank_current_setpoint = 285.0
 
-        self.data = (
-            str(self.address) + "," + str(self.heat_status) + "," + "285.0,280.0,1006,286,0,972"
-        )
-        #
+        self.tank_current = 280.0
+
+        self.power = 1006
+
+        self.frequency = 286
+
+        self.count_down = 0
+
+        self.count_up = 972
+
         # Power supply address, heat on (1=on, 0=off), set point amps, tank amps, power watts,
         # frequency kHz, count down timer (msec.), count up timer (msec.)
         #
@@ -33,11 +43,28 @@ class SimulatedAmbrellEasyHeat(StateMachineDevice):
         # HSINK TEMP, READY LED, HEAT LED, LIMIT LED, FAULT LED, TAP, MAX VOLTS, OVERLOAD,
         # TIMER, ANALOG INPUT
 
-        self.status = "220.0,53.6,421.2,87.9,62.8"
+        self.data = f"{self.address},{self.heat_status},{self.tank_current_setpoint},{self.tank_current},{self.power},{self.frequency},{self.count_down},{self.count_up}"
+
+
+        # Values in status string:
+
+        self.mains_voltage = 220.0
+
+        self.total_time = 53.6
+
+        self.max_power = 421.2
+
+        self.max_heatsink_temp = 87.9
+
+        self.max_enclosure_temp = 62.8
+
         # Unclear as to exactly what is and how it is returned from the physical PSU
         # as manual is ambiguous
         # e.g Mains voltage, total time, max. power out, max. heat sink temperature,
         # max. enclosure temperature
+
+        self.status = f"{self.mains_voltage},{self.total_time},{self.max_power},{self.max_heatsink_temp},{self.max_enclosure_temp}"
+
 
     def _get_state_handlers(self) -> dict:
         return {
--- system_tests/tests/tests.py
+++ system_tests/tests/tests.py
@@ -32,77 +32,90 @@ class AmbrellEasyHeatTests(unittest.TestCase):
 
     
     def test_WHEN_system_id_requested_THEN_value_returned(self):
-        expected_system_id = 12345
+        expected_system_id = 54321
+        self._lewis.backdoor_set_on_device("id", expected_system_id)
         self.ca.assert_that_pv_is(
             "ID", expected_system_id, timeout=10
         )  # timeout to cover 10s SCAN of record.  Value returned in ~9s.
 
     
     def test_WHEN_data_requested_THEN_address_returned_and_PV_populated(self):
-        expected_address = 3
+        expected_address = 4
+        self._lewis.backdoor_set_on_device("address", expected_address)
         self.ca.assert_that_pv_is(
             "ADDRESS:RBV", expected_address, timeout=2
         )  # timeout to cover 1s SCAN of record.  Value returned in <1s.
 
     
     def test_WHEN_data_requested_THEN_heat_status_returned_and_PV_populated(self):
-        expected_heat_status = "ON"
+        expected_heat_status = "OFF"
+        self._lewis.backdoor_set_on_device("heat_status", expected_heat_status)
         self.ca.assert_that_pv_is("HEAT:RBV", expected_heat_status, timeout=2)
 
     
     def test_WHEN_data_requested_THEN_tank_current_setpoint_returned_and_PV_populated(self):
-        expected_tank_current_setpoint = 285.0
+        expected_tank_current_setpoint = 185.0
+        self._lewis.backdoor_set_on_device("tank_set_point", expected_tank_current_setpoint)
         self.ca.assert_that_pv_is("TANK:CURR:SP:RBV", expected_tank_current_setpoint, timeout=2)
 
     
     def test_WHEN_data_requested_THEN_tank_current_returned_and_PV_populated(self):
-        expected_tank_current = 280.0
+        expected_tank_current = 180.0
+        self._lewis.backdoor_set_on_device("tank_current", expected_tank_current)
         self.ca.assert_that_pv_is("TANK:CURR:RBV", expected_tank_current, timeout=2)
 
     
     def test_WHEN_data_requested_THEN_power_returned_and_PV_populated(self):
-        expected_power = 1006
+        expected_power = 2006
+        self._lewis.backdoor_set_on_device("power", expected_power)
         self.ca.assert_that_pv_is("POWER:RBV", expected_power, timeout=2)
 
     
     def test_WHEN_data_requested_THEN_frequency_returned_and_PV_populated(self):
-        expected_frequency = 286
+        expected_frequency = 186
+        self._lewis.backdoor_set_on_device("frequency", expected_frequency)
         self.ca.assert_that_pv_is("FREQ:RBV", expected_frequency, timeout=2)
 
     
     def test_WHEN_data_requested_THEN_count_down_timer_returned_and_PV_populated(self):
-        expected_count_down_timer = 0
+        expected_count_down_timer = 15
+        self._lewis.backdoor_set_on_device("count_down", expected_count_down_timer)
         self.ca.assert_that_pv_is("TIME:DOWN:RBV", expected_count_down_timer, timeout=2)
 
     
     def test_WHEN_data_requested_THEN_count_up_timer_returned_and_PV_populated(self):
-        expected_count_up_timer = 972
+        expected_count_up_timer = 872
+        self._lewis.backdoor_set_on_device("count_up", expected_count_up_timer)
         self.ca.assert_that_pv_is("TIME:UP:RBV", expected_count_up_timer, timeout=2)
 
     #   Tests for record redirection from returned status string
 
     
     def test_WHEN_status_requested_THEN_mains_voltage_returned_and_PV_populated(self):
-        expected_mains_voltage = 220.0
+        expected_mains_voltage = 120.0
+        self._lewis.backdoor_set_on_device("mains_voltage", expected_mains_voltage)
         self.ca.assert_that_pv_is(
             "VOLT:MAINS:RBV", expected_mains_voltage, timeout=2
         )  # timeout to cover 1s SCAN of record.  Value returned in <1s.
 
     
     def test_WHEN_status_requested_THEN_total_time_returned_and_PV_populated(self):
-        expected_total_time = 53.6
+        expected_total_time = 43.6
+        self._lewis.backdoor_set_on_device("total_time", expected_total_time)
         self.ca.assert_that_pv_is("TIME:TOTAL:RBV", expected_total_time, timeout=2)
 
     
     def test_WHEN_status_requested_THEN_maximum_power_returned_and_PV_populated(self):
-        expected_maximum_power = 421.2
+        expected_maximum_power = 521.2
+        self._lewis.backdoor_set_on_device("max_power", expected_maximum_power)
         self.ca.assert_that_pv_is("POWER:MAX:RBV", expected_maximum_power, timeout=2)
 
     
     def test_WHEN_status_requested_THEN_maximum_heatsink_temperature_returned_and_PV_populated(
         self,
     ):
-        expected_maximum_heatsink_temperature = 87.9
+        expected_maximum_heatsink_temperature = 77.9
+        self._lewis.backdoor_set_on_device("max_heatsink_temp", expected_maximum_heatsink_temperature)
         self.ca.assert_that_pv_is(
             "TEMP:HEATSINK:MAX:RBV", expected_maximum_heatsink_temperature, timeout=2
         )
@@ -111,7 +124,8 @@ class AmbrellEasyHeatTests(unittest.TestCase):
     def test_WHEN_status_requested_THEN_maximum_enclosure_temperature_returned_and_PV_populated(
         self,
     ):
-        expected_maximum_enclosure_temperature = 62.8
+        expected_maximum_enclosure_temperature = 52.8
+        self._lewis.backdoor_set_on_device("max_enclosure_temp", expected_maximum_enclosure_temperature)
         self.ca.assert_that_pv_is(
             "TEMP:ENCLOSURE:MAX:RBV", expected_maximum_enclosure_temperature, timeout=2
         )
